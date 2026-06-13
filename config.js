5a9a092cdfa5c9b35cd14781be98178a96eda94d
Zelio <zelio@die-welt.com>
2026-06-13 18:32:21 +0200

    Temporär: mandatoryEmail auf zerotheprogamer@gmail.com für Test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js – Zentrale Konfiguration für die KAMA Leistungsnachweis-App
 // Diese Werte können angepasst werden, ohne index.html zu ändern.
 window.KAMA_CONFIG = {
-  mandatoryEmail: 'info@kama-services.eu',
+  mandatoryEmail: 'zerotheprogamer@gmail.com', // TODO: auf info@kama-services.eu ändern sobald Domain verifiziert + MAIL_MODE = live
   mailApiEndpoint: 'https://kama-mail-api.kama-services.workers.dev',
   mailApiToken: '',
 };
